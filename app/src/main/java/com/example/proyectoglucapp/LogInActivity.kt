3866bdb0fa1fedad4dbd6542fcdccca9d1721918
Juan Luis Casa <juanluismatalop@gmail.com>
2024-11-06 20:34:18 +0100

    commit creacion layout y inicio de sesion

--- app/src/main/java/com/example/proyectoglucapp/LogInActivity.kt
+++ app/src/main/java/com/example/proyectoglucapp/LogInActivity.kt
@@ -1,20 +1,40 @@
 package com.example.proyectoglucapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LogInActivity : AppCompatActivity() {
+
+    private val MYUSER = "usuario"
+    private val MYPASS = "contrasena"
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_log_in)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val etUsername = findViewById<EditText>(R.id.etUsuario)
+        val etPassword = findViewById<EditText>(R.id.etContrasenna)
+        val btnValidar = findViewById<Button>(R.id.Validar)
+
+        btnValidar.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            if (username == MYUSER && password == MYPASS) {
+                // Si es correcto, inicia el Activity Principal
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("username", username)
+                intent.putExtra("password", password)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
