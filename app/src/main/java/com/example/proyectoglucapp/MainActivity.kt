507260660520d0fc6a2e555b7b6f2ad23892a34b
julu <juanluismatalop@gmail.com>
2025-01-09 13:42:18 +0100

    version 1.4

--- app/src/main/java/com/example/proyectoglucapp/MainActivity.kt
+++ app/src/main/java/com/example/proyectoglucapp/MainActivity.kt
@@ -2,45 +2,78 @@ package com.example.proyectoglucapp
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.proyectoglucapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        drawerLayout = binding.drawerLayout
+        navView = binding.navView
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            binding.toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_noticias -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, NoticiasRecycler())
+                        .commit()
+                }
+                R.id.nav_ajustes -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, AjustesFragment())
+                        .commit()
+                }
+                R.id.nav_mis_datos -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, MisDatosFragment())
+                        .commit()
+                }
+                R.id.nav_logout -> {
+                    FirebaseAuth.getInstance().signOut()
+                    val intent = Intent(this, LogInActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            drawerLayout.closeDrawers()
+            true
+        }
+
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, NoticiasRecycler())
             .commit()
 
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val email = currentUser?.email
 
-        val usuario = intent.getStringExtra("usuario")
-        binding.Bienvenido.text = "Bienvenido, $usuario!"
-
-
-        /**binding.botonAjuste.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, AjustesFragment())
-                .commit()
-        }*/
-
-        /**binding.botonCalculadora.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, CalculadoraFragment())
-                .commit()
-        }*/
-
-        /**binding.botonMisDatos.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, MisDatosFragment())
-                .commit()
-        }*/
+        if (email != null) {
+            val username = email.substringBefore("@")
+            binding.Bienvenido.text = "Bienvenido, $username!"
+        } else {
+            binding.Bienvenido.text = "Bienvenido, Usuario!"
+        }
 
         binding.boronCerrarSesion.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -50,6 +83,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+
 
 
 
