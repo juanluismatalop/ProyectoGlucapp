1cd0bf0e16a38ef904db7378a76ef33e6edf38ef
julu <juanluismatalop@gmail.com>
2025-02-13 14:19:32 +0100

    video

--- app/src/main/java/com/example/proyectoglucapp/ui/views/fragment/VideoFragment.kt
+++ app/src/main/java/com/example/proyectoglucapp/ui/views/fragment/VideoFragment.kt
@@ -1,17 +1,35 @@
 package com.example.proyectoglucapp.ui.views.fragment
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.VideoView
 import androidx.fragment.app.Fragment
 import com.example.proyectoglucapp.R
 
-class VideoFragment: Fragment() {
+class VideoFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_video, container, false)
+        val view = inflater.inflate(R.layout.fragment_video, container, false)
+
+        val videoView: VideoView = view.findViewById(R.id.videoView)
+
+        val videoUri: Uri = Uri.parse("android.resource://" + requireContext().packageName + "/" + R.raw.video_ejemplo1)
+
+        videoView.setVideoURI(videoUri)
+
+        videoView.start()
+
+        videoView.setOnCompletionListener {
+            println("El video ha terminado.")
+        }
+
+        return view
     }
 }
+
